8364b24c572ab7462224d62342320effa4338049
Andy Smith <andyrsmith@gmail.com>
2024-01-27 13:41:05 +0000

    Incremental.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 use std::env;
 use std::fmt::Display;
+use std::path::PathBuf;
+
+
+fn print_lines_from_file(f : &PathBuf) {
+    let contents = std::fs::read_to_string(f).expect("Something went wrong reading the file");
+    for line in contents.lines() {
+        println!("{}", line);
+    }
+}
+
+fn read_file<const N : usize>(f : &str) -> [[bool; N]; N]
+{
+    let contents = std::fs::read_to_string(f).expect("Something went wrong reading the file");
+    let mut a = [[Default::default();N]; N];
+    for (line, y) in contents.lines().zip(0..N) {
+        // Remove leading and trailing '|' character from line
+        let line = line.trim_matches('|');
+        for (c, x) in line.chars().zip(0..N) {
+            a[y][x] = c == 'X';
+        }
+    }
+    a
+}
+
+
+
 
 fn main() {
-    println!("Hello, world!\nI'm a rustacean");
-
     for i in (0..20).step_by(2) {
         println!("i is {:>2}", i);
     }
 
-    println!("{:?}", env::current_dir());
-
     // print current directory
     println!("{:?}", env::current_dir());
+
+    let resourceDir = env::current_dir().unwrap().join("resources");
 
     // define a 'squarable' trait
     trait Squarable {
@@ -33,11 +57,16 @@ fn main() {
 
     // Define 2d array of integers
 
-    struct SquareArray<T : Default, const N : usize> {
+    struct SquareArray<T : Default, const N : usize>  {
         array: [[T; N]; N],
     }
 
     impl<T : Default + Copy, const N : usize> SquareArray<T,N> {
+        fn supply() -> SquareArray<u8, N> {
+            SquareArray<u8, N> {
+                 [[u8; N]; N],
+            };
+        }
         fn new() -> Self {
             SquareArray {
                 // initialize array
@@ -67,7 +96,10 @@ fn main() {
     }
 
     // Define a square array of size 10
-    let square_array = SquareArray::<u8, 10>::new();
+    let square_array = SquareArray::<bool, 10>::new();
+    let aux_array = SquareArray::<u8, 10>::new();
+
+
 
     square_array.print();
 }
