2b92060bf1898bcec1358738b8245cf069378a95
Andy Smith <andyrsmith@gmail.com>
2024-01-27 06:24:24 +0000

    Incremental.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::env;
+use typenum::Unsigned;
+
+static HELLO : &str = "HELLO";
 
 fn main() {
     println!("Hello, world!\nI'm a rustacean");
@@ -29,4 +32,41 @@ fn main() {
 
     println!("{} squared is {}", 32u32, 32u32.square());
     println!("{} squared is {}", 32u16, 32u16.square());
+
+    // Define 2d array of integers
+
+    struct SquareArray<T : typenum::Unsigned> {
+        size: usize,
+        array: [[i32; T::USIZE]; T::USIZE],
+    }
+
+    impl<T> SquareArray<T> {
+        fn new(size: usize) -> SquareArray<T> {
+            SquareArray {
+                size,
+                array: [[0; T::USIZE]; T::USIZE],
+            }
+        }
+    }
+
+    // Define a 'Printable' trait
+    trait Printable {
+        fn print(&self);
+    }
+
+
+    // Implement printable for square array
+    impl<T> Printable for SquareArray<T>
+    where
+        T: typenum::Unsigned,
+    {
+        fn print(&self) {
+            for i in 0..self.size {
+                for j in 0..self.size {
+                    print!("{} ", self.array[i][j]);
+                }
+                println!();
+            }
+        }
+    }
 }
