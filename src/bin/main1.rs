fd3d45e65cdc6a013ef7b1321fc22992c90ae7c2
Andy Smith <andyrsmith@gmail.com>
2024-02-05 12:05:11 +0000

    Got it I think.

--- src/bin/main1.rs
+++ src/bin/main1.rs
@@ -10,26 +10,40 @@ struct SquareArray<T: Default + Repr> {
     array: [[T; N]; N],
 }
 
-trait DumpableWithMax {
-    fn dump_with_max(&self, max_data: (usize, usize, i32));
-}
+#[derive(Debug)]
+struct BestResults(u8, Vec<(usize, usize)>);
+
+#[derive(Debug)]
+struct BestResult(u8, usize, usize);
+
 
 impl<T: Default + Copy + Repr> SquareArray<T> {
-    fn create_aux(&self) -> SquareArray<i32> {
-        SquareArray::<i32> {
+    fn create_aux(&self) -> SquareArray<u8> {
+        SquareArray::<u8> {
             array: [[Default::default(); N]; N],
         }
     }
 
-    fn print(&self) {
+    fn print(&self, best: Option<&BestResult>) {
         for i in 0..N {
             for j in 0..N {
-                print!("{} ", self.array[i][j].repr());
+                match best {
+                    Some(BestResult(_n, _y, _x)) => {
+                        let (n, y, x) = (*_n as usize, *_y, *_x);
+                        let y_range_start = if y >= n { y - n + 1 } else { 0 };
+                        let x_range_start = if x >= n { x - n + 1 } else { 0 };
+
+                        print!("{}", if (y_range_start..=y).contains(&i) && (x_range_start..=x).contains(&j)
+                            {"X "} else {". "});
+                    }
+                    _ => {
+                        print!("{} ", self.array[i][j].repr());
+                    }
+                }
             }
             println!();
         }
     }
-
 
     fn new() -> Self {
         SquareArray {
@@ -39,19 +53,6 @@ impl<T: Default + Copy + Repr> SquareArray<T> {
     }
 }
 
-impl DumpableWithMax for SquareArray<i32> {
-    fn dump_with_max(&self, max_data: (usize, usize, i32)) {
-        let (y, x,  num) = max_data;
-        let n2 = num as usize;
-        let mut ret = self.clone();
-        for yoffset  in y  - n2 + 1 ..=y {
-            for xoffset in x - n2 + 1..=x {
-                ret.array[yoffset][xoffset] -= 1;
-            }
-        }
-        self.print();
-    }
-}
 // Print current directory
 //println!("Current directory is {:?}", env::current_dir());
 
@@ -69,21 +70,12 @@ impl Repr for bool {
     }
 }
 
-impl Repr for usize {
+impl Repr for u8 {
     fn repr(&self) -> String {
         return format!("{}", self);
     }
 }
 
-impl Repr for i32 {
-    fn repr(&self) -> String {
-        return if *self == -1 {
-            "X".to_string()
-        } else {
-            " ".to_string()
-        };
-    }
-}
 
 // Define a 'Printable' trait
 trait Printable {
@@ -108,44 +100,14 @@ fn read_file_from_path(f: PathBuf) -> SquareArray<bool>
 }
 
 fn main() {
-    for i in (0..20).step_by(2) {
-        println!("i is {:>2}", i);
-    }
-
-    // print current directory
-    //println!("{:?}", env::current_dir());
-
     let resource_dir = env::current_dir().unwrap().join("resources");
 
     println!("Resource dir is {:?}", resource_dir);
 
-    // define a 'squarable' trait
-    trait Squarable {
-        fn square(&self) -> Self;
-    }
-
-    // a macro implementing the trait for any type
-    impl<T> Squarable for T
-        where
-            T: std::ops::Mul<Output=T> + Copy,
-    {
-        fn square(&self) -> Self {
-            *self * *self
-        }
-    }
-
-    println!("{} squared is {}", 32u32, 32u32.square());
-    println!("{} squared is {}", 32u16, 32u16.square());
 
     let square_array = read_file_from_path(
         resource_dir.join(PathBuf::from("dat1.txt")));
     let mut aux_array = square_array.create_aux();
-
-    #[derive(Debug)]
-    struct BestResults(i32, Vec<(usize, usize)>);
-
-    #[derive(Debug)]
-    struct BestResult(i32, Vec<(usize, usize)>);
 
     let none = None;
     let mut best_result: Option<BestResults> = none;
@@ -158,8 +120,6 @@ fn main() {
                 } else {
                     aux_array.array[y][x] = 1 + aux_array.array[y - 1][x - 1].min(aux_array.array[y - 1][x].min(aux_array.array[y][x - 1]));
                 }
-
-                // Match against best_result
 
                 let current_val = aux_array.array[y][x];
 
@@ -181,14 +141,17 @@ fn main() {
             }
         }
     }
-    square_array.print();
-    aux_array.print();
+    square_array.print(None);
+    println!();
+    aux_array.print(None);
+    println!();
 
     let BestResults(num, results) = best_result.as_ref().expect("Error");
 
-    results.iter().for_each(|(y,x)| {
-        aux_array.dump_with_max((*y, *x, *num));
-        println!();
+    results.iter().for_each(|(y, x)| {
+        let br = BestResult(*num, *y, *x);
+        println!("====================================");
+        aux_array.print(Some(&br));
         println!();
     });
 
